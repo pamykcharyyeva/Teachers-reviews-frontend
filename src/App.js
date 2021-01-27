29c44b49fbf2deda8f3abf6c2b87530cbc1430d8
pamykcharyyeva <pamyk.ch@gmail.com>
2021-01-27 05:17:34 -0600

    changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-import { connect } from 'react-redux'
-import reducer from './reducer'
+import logo from './logo.svg';
 import './App.css';
-import AllTeachersPage from './containers/AllTeachersPage'
 
-class App extends React.Component {
-   render(){
-     return (
-      <Router>
-        <div>
-         <AllTeachersPage/>
-     
-        </div>
-      </Router>
-     )
-   }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-function mapStateToProps(state){
-  return(state)
-  
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
